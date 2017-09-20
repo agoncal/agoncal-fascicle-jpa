5026d537d2bbe70ea4dcb84b8cfab2bdc8884202
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-20 15:38:10 +0200

    mapping

---
+++ mapping/src/main/java/org/agoncal/fascicle/jpa/mapping/ex18/Address.java
@@ -0,0 +1,97 @@
+package org.agoncal.fascicle.jpa.mapping.ex18;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+// tag::adocsnippet[]
+@Embeddable
+@Access(AccessType.PROPERTY)
+public class Address {
+
+  private String street1;
+  private String street2;
+  private String city;
+  private String state;
+  private String zipcode;
+  private String country;
+
+  // Constructors
+  // tag::adocskip[]
+
+  public Address() {
+  }
+
+  public Address(String street1, String street2, String city, String state, String zipcode, String country) {
+    this.street1 = street1;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.country = country;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+  // end::adocskip[]
+
+  @Column(nullable = false)
+  public String getStreet1() {
+    return street1;
+  }
+
+  public void setStreet1(String street1) {
+    this.street1 = street1;
+  }
+
+  public String getStreet2() {
+    return street2;
+  }
+
+  public void setStreet2(String street2) {
+    this.street2 = street2;
+  }
+
+  @Column(nullable = false, length = 50)
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  @Column(length = 3)
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  @Column(name = "zip_code", length = 10)
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+}
+// end::adocsnippet[]
