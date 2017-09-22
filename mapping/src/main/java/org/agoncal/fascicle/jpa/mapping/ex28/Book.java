d9d38b8b639bd08ae8cf485ae12e5561d8c53afe
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-22 08:42:17 +0200

    mapping

---
+++ mapping/src/main/java/org/agoncal/fascicle/jpa/mapping/ex28/Book.java
@@ -0,0 +1,70 @@
+package org.agoncal.fascicle.jpa.mapping.ex28;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+// tag::adocsnippet[]
+@Entity
+public class Book extends Item {
+
+  private String isbn;
+  private String publisher;
+  private Integer nbOfPage;
+  private Boolean illustrations;
+
+  // Constructors, getters, setters
+  // tag::adocskip[]
+
+  public Book() {
+  }
+
+  public Book(String title, Float price, String description, String isbn, String publisher, Integer nbOfPage, Boolean illustrations) {
+    super(title, price, description);
+    this.isbn = isbn;
+    this.publisher = publisher;
+    this.nbOfPage = nbOfPage;
+    this.illustrations = illustrations;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public Integer getNbOfPage() {
+    return nbOfPage;
+  }
+
+  public void setNbOfPage(Integer nbOfPage) {
+    this.nbOfPage = nbOfPage;
+  }
+
+  public Boolean getIllustrations() {
+    return illustrations;
+  }
+
+  public void setIllustrations(Boolean illustrations) {
+    this.illustrations = illustrations;
+  }
+  // end::adocskip[]
+}
+// end::adocsnippet[]
---
+++ mapping/src/test/java/org/agoncal/fascicle/jpa/mapping/ex28/ItemTest.java
@@ -0,0 +1,39 @@
+package org.agoncal.fascicle.jpa.mapping.ex28;
+
+import org.agoncal.fascicle.jpa.mapping.AbstractPersistentTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class ItemTest extends AbstractPersistentTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+  @Test
+  public void shouldCreateSeveralItems() throws Exception {
+
+    Item item = new Item("Junk", 52.50f, "A piece of junk");
+    CD cd01 = new CD("St Pepper", 12.80f, "Beatles master piece", "Apple", 1, 53.32f, "Pop/Rock");
+    CD cd02 = new CD("Love SUpreme", 20f, "John Coltrane love moment", "Blue Note", 2, 87.45f, "Jazz");
+    Book book01 = new Book("H2G2", 21f, "Best IT book", "123-456-789", "Pinguin", 321, false);
+    Book book02 = new Book("The Robots of Dawn", 37.5f, "Robots, again and again", "0-553-29949-2 ", "Foundation", 264, true);
+    tx.begin();
+    em.persist(item);
+    em.persist(cd01);
+    em.persist(cd02);
+    em.persist(book01);
+    em.persist(book02);
+    tx.commit();
+    assertNotNull(item.getId(), "Item ID should not be null");
+    assertNotNull(cd01.getId(), "CD1 ID should not be null");
+    assertNotNull(cd02.getId(), "CD2 ID should not be null");
+    assertNotNull(book01.getId(), "Book1 ID should not be null");
+    assertNotNull(book02.getId(), "Book2 ID should not be null");
+  }
+}
