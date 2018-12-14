8913d2edc88ec6984cc7e7152117f81972df7155
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-12-14 21:02:01 +0100

    Refactoring modules

---
+++ mapping/Column.java
@@ -0,0 +1,27 @@
+package org.agoncal.fascicle.jpa.mapping;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+// @formatter:off
+// tag::adocSnippet[]
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
+public @interface Column {
+
+  String  name()       default "";
+  boolean unique()     default false;
+  boolean nullable()   default true;
+  boolean insertable() default true;
+  boolean updatable()  default true;
+  String  columnDefinition() default "";
+  String  table()      default "";
+  int     length()     default 255;
+  int     precision()  default 0; // decimal precision
+  int     scale()      default 0; // decimal scale
+}
+// end::adocSnippet[]
