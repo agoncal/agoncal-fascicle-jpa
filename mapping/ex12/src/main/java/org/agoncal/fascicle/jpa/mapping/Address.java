a86b8aa685de00b9c932218d606db193ef5af971
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-12-16 17:26:55 +0100

    XML doc

---
+++ mapping/ex12/src/main/java/org/agoncal/fascicle/jpa/mapping/Address.java
@@ -0,0 +1,89 @@
+package org.agoncal.fascicle.jpa.mapping;
+
+import javax.persistence.Embeddable;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Embeddable
+public class Address {
+
+  private String street1;
+  private String street2;
+  private String city;
+  private String state;
+  private String zipcode;
+  private String country;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Address() {
+  }
+
+  public Address(String street1, String street2, String city, String state, String zipcode, String country) {
+    this.street1 = street1;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.zipcode = zipcode;
+    this.country = country;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getStreet1() {
+    return street1;
+  }
+
+  public void setStreet1(String street1) {
+    this.street1 = street1;
+  }
+
+  public String getStreet2() {
+    return street2;
+  }
+
+  public void setStreet2(String street2) {
+    this.street2 = street2;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
