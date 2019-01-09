de2cef44571d3bdab91eb3663e4f0f0b81a583d6
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-01-09 22:19:31 +0100

    Callbacks

---
+++ callbacks-listeners/ex02/src/main/java/org/agoncal/fascicle/jpa/callbackslisteners/Customer.java
@@ -0,0 +1,117 @@
+package org.agoncal.fascicle.jpa.callbackslisteners;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@EntityListeners({DataValidationListener.class, AgeCalculationListener.class})
+@Entity
+public class Customer {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phoneNumber;
+  private LocalDate dateOfBirth;
+  @Transient
+  private Integer age;
+  private LocalDateTime creationDate;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Customer() {
+  }
+
+  public Customer(String firstName, String lastName, String email, String phoneNumber, LocalDate dateOfBirth, LocalDateTime creationDate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.dateOfBirth = dateOfBirth;
+    this.creationDate = creationDate;
+  }
+
+  public Customer(String firstName, String lastName, String email, String phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+  }
+
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public LocalDateTime getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(LocalDateTime creationDate) {
+    this.creationDate = creationDate;
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ callbacks-listeners/ex02/src/test/java/org/agoncal/fascicle/jpa/callbackslisteners/CustomerTest.java
@@ -0,0 +1,73 @@
+package org.agoncal.fascicle.jpa.callbackslisteners;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class CustomerTest extends AbstractPersistentTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+
+  @Test
+  public void shouldCheckTheAgeOfTheCustomer() throws Exception {
+
+    // Instanciates an object
+    Customer customer = new Customer("John", "Smith", "jsmith@gmail.com", "1234565", LocalDate.now(), LocalDateTime.now());
+    assertFalse(em.contains(customer));
+
+    // Persists the object
+    tx.begin();
+    em.persist(customer);
+    tx.commit();
+    assertTrue(em.contains(customer), "should be in the persistence context after persisting");
+    assertEquals(new Integer(0), customer.getAge());
+
+    // Finds the object
+    customer = em.find(Customer.class, customer.getId());
+    assertTrue(em.contains(customer), "should be in the persistence context after finding");
+    assertEquals(new Integer(0), customer.getAge());
+
+    // Removes the entity
+    tx.begin();
+    em.remove(customer);
+    tx.commit();
+    assertFalse(em.contains(customer), "should not be in the persistence context after removing");
+  }
+
+  @Test
+  public void shouldThrowAnExceptionBecauseFirstNameIsNull() throws Exception {
+
+    // Instanciates an object
+    Customer customer = new Customer(null, "Smith", "jsmith@gmail.com", "1234565", LocalDate.now(), LocalDateTime.now());
+
+    assertThrows(IllegalArgumentException.class,
+      () -> {
+        // Persist the object
+        em.persist(customer);
+      });
+  }
+
+  @Test
+  public void shouldThrowAnExceptionBecauseLastNameIsNull() throws Exception {
+
+    // Instanciates an object
+    Customer customer = new Customer("John", null, "jsmith@gmail.com", "1234565", LocalDate.now(), LocalDateTime.now());
+
+    assertThrows(IllegalArgumentException.class,
+      () -> {
+        // Persist the object
+        em.persist(customer);
+      });
+  }
+}
