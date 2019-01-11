2eb5059ccaedc93c4ecc5d6d423943fc5fbbdcb3
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-01-11 16:16:33 +0100

    Spring

---
+++ integrating/spring/src/main/java/org/agoncal/fascicle/jpa/integrating/spring/AddressEndpoint.java
@@ -0,0 +1,37 @@
+package org.agoncal.fascicle.jpa.integrating.spring;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// tag::adocSnippet[]
+@RestController
+public class AddressEndpoint {
+
+  private final AddressRepository addressRepository;
+
+  public AddressEndpoint(AddressRepository addressRepository) {
+    this.addressRepository = addressRepository;
+  }
+
+  @PostMapping("/addresses")
+  public Address createAddress(@RequestBody Address address) {
+    return addressRepository.save(address);
+  }
+
+  @GetMapping(value = "/addresses/count")
+  public Long countAll() {
+    return addressRepository.count();
+  }
+
+  @GetMapping(value = "/addresses/country/{country}")
+  public List<Address> getAddressesByCountry(@PathVariable String country) {
+    return addressRepository.findAllByCountry(country);
+  }
+
+  @GetMapping(value = "/addresses/like/{zip}")
+  public List<Address> getAddressesLikeZip(@PathVariable String zip) {
+    return addressRepository.findAllLikeZip(zip);
+  }
+}
+// end::adocSnippet[]
---
+++ integrating/spring/src/main/java/org/agoncal/fascicle/jpa/integrating/spring/AddressRepository.java
@@ -0,0 +1,18 @@
+package org.agoncal.fascicle.jpa.integrating.spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+  List<Address> findAllByCountry(@Param("country") String country);
+
+  @Query("SELECT a FROM Address a WHERE a.zipcode LIKE %:zip%")
+  List<Address> findAllLikeZip(@Param("zip") String zip);
+}
