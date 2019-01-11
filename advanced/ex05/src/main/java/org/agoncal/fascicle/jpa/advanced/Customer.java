b169fdeeb38e9a6241eaeb379929129a28a6c2c6
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-01-11 12:22:00 +0100

    Add cache

---
+++ advanced/ex05/src/main/java/org/agoncal/fascicle/jpa/advanced/Customer.java
@@ -0,0 +1,71 @@
+package org.agoncal.fascicle.jpa.advanced;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Entity
+@Cacheable(true)
+public class Customer {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Customer() {
+  }
+
+  public Customer(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ advanced/ex05/src/test/java/org/agoncal/fascicle/jpa/advanced/CachingTest.java
@@ -0,0 +1,84 @@
+package org.agoncal.fascicle.jpa.advanced;
+
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Cache;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class CachingTest extends AbstractPersistentTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+
+  @Test
+  public void shouldCheckThatAddressIsCacheableEqualsFalse() throws Exception {
+
+    Address address = new Address("Ritherdon Rd", "London", "8QE", "UK");
+
+    // Persist the object
+    tx.begin();
+    em.persist(address);
+    tx.commit();
+
+    assertNotNull(address.getId());
+
+    Cache cache = emf.getCache();
+
+    // Address should not be in the cache
+    assertFalse(cache.contains(Address.class, address.getId()));
+  }
+
+
+  @Test
+  public void shouldCheckThatCustomerIsCacheableEqualsTrue() throws Exception {
+
+    // tag::adocSnippet[]
+    Customer customer = new Customer("Patricia", "Jane", " plecomte@mail.com ");
+
+    // Persist the object
+    tx.begin();
+    em.persist(customer);
+    tx.commit();
+
+    assertNotNull(customer.getId());
+
+    // Uses the EntityManagerFactory to get the Cache
+    Cache cache = emf.getCache();
+
+    // Customer should be in the cache
+    assertTrue(cache.contains(Customer.class, customer.getId()));
+
+    // Removes the Customer entity from the cache
+    cache.evict(Customer.class);
+
+    // After clearing the cache Customer should not be in the cache anymore
+    assertFalse(cache.contains(Customer.class, customer.getId()));
+    // end::adocSnippet[]
+  }
+
+  @Test
+  public void shouldCheckThatBookIsNotCacheableByDefault() throws Exception {
+
+    Book book = new Book(getRandomId(), "The Hitchhiker's Guide to the Galaxy", 12.5F, "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.", "1-84023-742-2", 354, false);
+
+    // Persist the object
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+
+    assertNotNull(book.getId());
+
+    Cache cache = emf.getCache();
+
+    // Customer should not be in the cache by default
+    assertFalse(cache.contains(Customer.class, book.getId()));
+  }
+}
