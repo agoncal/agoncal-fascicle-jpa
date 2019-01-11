3b04626cfc6459e9d3aa8fae1b0dfccbc7250d0b
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-01-11 13:21:12 +0100

    Version

---
+++ advanced/ex06/src/main/java/org/agoncal/fascicle/jpa/advanced/Book.java
@@ -0,0 +1,99 @@
+package org.agoncal.fascicle.jpa.advanced;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+@Entity
+public class Book {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  @Version
+  private Integer version;
+  private String title;
+  private Float price;
+  private String description;
+  private String isbn;
+
+  // Constructors, getters, setters
+  // tag::adocSkip[]
+
+  public Book() {
+  }
+
+  public Book(String title, Float price, String description, String isbn) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.isbn = isbn;
+  }
+
+  // ======================================
+  // =           Public Methods           =
+  // ======================================
+
+  public void raisePriceByTwoDollars() {
+    price = price + 2;
+  }
+
+  public void raisePriceByFiveDollars() {
+    price = price + 5;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Long getId() {
+    return id;
+  }
+
+  public Integer getVersion() {
+    return version;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Float getPrice() {
+    return price;
+  }
+
+  public void setPrice(Float price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ advanced/ex06/src/test/java/org/agoncal/fascicle/jpa/advanced/VersionTest.java
@@ -0,0 +1,100 @@
+package org.agoncal.fascicle.jpa.advanced;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.LockModeType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class VersionTest extends AbstractPersistentTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+
+  @Test
+  public void shouldCreateABook() throws Exception {
+
+    // Creates a book, version number should be 1
+    Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F, "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.", "1-84023-742-2");
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+    assertNotNull(book.getId(), "ID should not be null");
+    assertEquals(new Integer(1), book.getVersion(), "Version number should be 1");
+
+    // Updates the same book
+    tx.begin();
+    book = em.find(Book.class, book.getId());
+    book.setDescription("new description");
+    tx.commit();
+    assertEquals(new Integer(2), book.getVersion(), "Version number should be 2");
+  }
+
+  @Test
+  public void shouldCreateABookAndRaisePrice() throws Exception {
+
+    // Creates a book, version number should be 1
+    Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F, "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.", "1-84023-742-2");
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+    assertNotNull(book.getId(), "ID should not be null");
+    assertEquals(new Integer(1), book.getVersion(), "Version number should be 1");
+
+    // Updates the same book
+    tx.begin();
+    book.raisePriceByTwoDollars();
+    tx.commit();
+    assertEquals(new Integer(2), book.getVersion(), "Version number should be 2");
+  }
+
+  @Test
+  public void shouldReadThenLock() throws Exception {
+
+    // Creates a book, version number should be 1
+    Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F, "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.", "1-84023-742-2");
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+    assertNotNull(book.getId(), "ID should not be null");
+    assertEquals(new Integer(1), book.getVersion(), "Version number should be 1");
+
+    // Reads then locks
+    tx.begin();
+    book = em.find(Book.class, book.getId());
+    em.lock(book, LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+    book.raisePriceByTwoDollars();
+    tx.commit();
+    assertEquals(new Integer(2), book.getVersion(), "Version number should be 2");
+    assertEquals(new Float(14.5), book.getPrice(), "Price shouls have been raise by 2 dollars");
+  }
+
+  @Test
+  public void shouldReadAndLock() throws Exception {
+
+    // Creates a book, version number should be 1
+    Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F, "The Hitchhiker's Guide to the Galaxy is a science fiction comedy series created by Douglas Adams.", "1-84023-742-2");
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+    assertNotNull(book.getId(), "ID should not be null");
+    assertEquals(new Integer(1), book.getVersion(), "Version number should be 1");
+
+    // Reads then locks
+    tx.begin();
+    book = em.find(Book.class, book.getId(), LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+    book.raisePriceByTwoDollars();
+    tx.commit();
+    assertEquals(new Integer(2), book.getVersion(), "Version number should be 2");
+    assertEquals(new Float(14.5), book.getPrice(), "Price shouls have been raise by 2 dollars");
+  }
+}
