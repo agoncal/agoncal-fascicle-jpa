2e65d0163a4f323db7dd2a2dc67152604438b48e
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-01-16 12:35:44 +0100

    Putting Together

--- putting-together/src/test/java/org/agoncal/fascicle/jpa/puttingtogether/BookTest.java
+++ putting-together/src/test/java/org/agoncal/fascicle/jpa/puttingtogether/BookTest.java
@@ -46,12 +46,60 @@ public class BookTest {
     // tag::shouldCreateABook[]
     Book book = new Book().title("Java EE 7").price(23.5F).isbn("1-84023-742-2").nbOfPages(354);
 
-    // Persists the book to the database
     tx.begin();
     em.persist(book);
     tx.commit();
     assertNotNull(book.getId(), "ID should not be null");
+
+    List<Book> allBooks = em.createNamedQuery("findAllBooks", Book.class).getResultList();
+    assertTrue(allBooks.size() >= 1);
     // end::shouldCreateABook[]
+  }
+
+  @Test
+  public void shouldCreateABookWithTagsAndChapters() throws Exception {
+
+    // tag::shouldCreateABookWithTagsAndChapters[]
+    Book book = new Book().title("Java EE 7").price(23.5F).isbn("1-84023-742-4").nbOfPages(354);
+    book.tag("java ee").tag("java").tag("enterprise");
+    book.chapter(1, new Chapter().title("Bean Validation"));
+    book.chapter(2, new Chapter().title("CDI"));
+    book.chapter(3, new Chapter().title("JPA"));
+    book.chapter(4, new Chapter().title("EJB"));
+
+    // Persists the book to the database
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+
+    // Checks the book
+    Book foundBook = em.find(Book.class, book.getId());
+    assertEquals(3, foundBook.getTags().size());
+    assertEquals(4, foundBook.getChapters().size());
+    // end::shouldCreateABookWithTagsAndChapters[]
+  }
+
+  @Test
+  public void shouldCreateABookWithTwoAuthors() throws Exception {
+
+    // tag::shouldCreateABookWithTwoAuthors[]
+    Author deepu = new Author().firstName("Deepu").lastName("Sasidharan");
+    Author sendil = new Author().firstName("Sendil").lastName("Kumar");
+    Book book = new Book().title("Full Stack Development with JHipster").price(23.5F).isbn("5-84023-742-5").nbOfPages(354).author(deepu).author(sendil);
+    deepu.book(book);
+    sendil.book(book);
+
+    // Persists the book to the database
+    tx.begin();
+    em.persist(book);
+    em.persist(deepu);
+    em.persist(sendil);
+    tx.commit();
+
+    // Checks the book
+    Book foundBook = em.find(Book.class, book.getId());
+    assertEquals(2, foundBook.getAuthors().size());
+    // end::shouldCreateABookWithTwoAuthors[]
   }
 
   @Test
@@ -85,29 +133,6 @@ public class BookTest {
   }
 
   @Test
-  public void shouldCreateABookWithTagsAndChapters() throws Exception {
-
-    // Creates an instance of book
-    Book book = new Book().title("Java EE 7").price(23.5F).isbn("1-84023-742-4").nbOfPages(354);
-    book.tag("java ee").tag("java").tag("enterprise");
-    book.chapter(1, new Chapter().title("Bean Validation"));
-    book.chapter(2, new Chapter().title("CDI"));
-    book.chapter(3, new Chapter().title("JPA"));
-    book.chapter(4, new Chapter().title("EJB"));
-
-    // Persists the book to the database
-    tx.begin();
-    em.persist(book);
-    tx.commit();
-    assertNotNull(book.getId(), "ID should not be null");
-
-    // Checks the book
-    Book foundBook = em.find(Book.class, book.getId());
-    assertEquals(3, foundBook.getTags().size());
-    assertEquals(4, foundBook.getChapters().size());
-  }
-
-  @Test
   public void shouldCreateABookWithAnAuthor() throws Exception {
 
     // Creates an instance of book
@@ -125,29 +150,6 @@ public class BookTest {
     // Checks the book
     Book foundBook = em.find(Book.class, book.getId());
     assertEquals(1, foundBook.getAuthors().size());
-  }
-
-  @Test
-  public void shouldCreateABookWithTwoAuthors() throws Exception {
-
-    // Creates an instance of book
-    Author deepu = new Author().firstName("Deepu").lastName("Sasidharan");
-    Author sendil = new Author().firstName("Sendil").lastName("Kumar");
-    Book book = new Book().title("Full Stack Development with JHipster").price(23.5F).isbn("5-84023-742-5").nbOfPages(354).author(deepu).author(sendil);
-    deepu.book(book);
-    sendil.book(book);
-
-    // Persists the book to the database
-    tx.begin();
-    em.persist(book);
-    em.persist(deepu);
-    em.persist(sendil);
-    tx.commit();
-    assertNotNull(book.getId(), "ID should not be null");
-
-    // Checks the book
-    Book foundBook = em.find(Book.class, book.getId());
-    assertEquals(2, foundBook.getAuthors().size());
   }
 
   @Test
