b4435a885bc2b6d31440e73057181d984781612d
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-04-09 23:18:07 +0200

    setFirstResult

--- querying/ex01/src/test/java/org/agoncal/fascicle/jpa/querying/DynamicQueriesTest.java
+++ querying/ex01/src/test/java/org/agoncal/fascicle/jpa/querying/DynamicQueriesTest.java
@@ -199,6 +199,14 @@ public class DynamicQueriesTest extends AbstractPersistentTest {
   }
 
   @Test
+  public void adocQueryAll() throws Exception {
+    TypedQuery<Customer> typedQuery = em.createQuery(
+      "SELECT c FROM Customer c", Customer.class);
+    List<Customer> customers = typedQuery.getResultList();
+    assertEquals(7, customers.size());
+  }
+
+  @Test
   public void adocQueryMax() throws Exception {
     // tag::adocQueryMax[]
     TypedQuery<Customer> typedQuery = em.createQuery(
@@ -207,6 +215,18 @@ public class DynamicQueriesTest extends AbstractPersistentTest {
     List<Customer> customers = typedQuery.getResultList();
     // end::adocQueryMax[]
     assertEquals(5, customers.size());
+  }
+
+  @Test
+  public void adocQueryFirstResult() throws Exception {
+    // tag::adocQueryFirstResult[]
+    TypedQuery<Customer> typedQuery = em.createQuery(
+      "SELECT c FROM Customer c", Customer.class);
+    typedQuery.setFirstResult(3);
+    typedQuery.setMaxResults(10);
+    List<Customer> customers = typedQuery.getResultList();
+    // end::adocQueryFirstResult[]
+    assertEquals(4, customers.size());
   }
 }
 
